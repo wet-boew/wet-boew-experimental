dd623e50d67578ea7f07b903eb6e45ee92d8466b
Mario Bonito <masterbee@Marios-Place.local>
2018-08-30 20:54:58 -0400

    removed experimental build tools

--- stacks/src/build.js
+++
@@ -1,9 +0,0 @@
-({
-   baseUrl: ".",
-   include: [ "wb5", 'module/aria', 'module/broadcast', "module/core/date", "module/core/pad", "module/core/number"],
-   name: "almond",
-   wrap: true,
-   findNestedDependencies: true,
-   optimize: "uglify2",
-   out: "../build/js/wb5.js"
-})
