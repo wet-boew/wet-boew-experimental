7a25b70236449f8d5080a02bb159d98ec279bf29
Mario Bonito <masterbee@mobile-hive.local>
2019-09-03 23:53:52 -0400

    initial structure and base files

---
+++ src/wet-boew.js
@@ -0,0 +1,10 @@
+// =============
+// = Polyfills =
+// =============
+
+var polyfills = [],
+	lang = ( document.documentElement.lang ) ? document.documentElement.lang : "en" ;
+
+require([], function() {
+    console.log( "WET 5 lives..");
+});
